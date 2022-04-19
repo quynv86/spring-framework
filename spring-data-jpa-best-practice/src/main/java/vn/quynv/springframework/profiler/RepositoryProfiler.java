df6ecf70af5015c84ad50fc299c9083abe61ae1b
quynv <quynv@Nguyens-MacBook-Pro.local>
2022-04-19 23:03:21 +0700

    Adding AOP to profile Repository

---
+++ spring-data-jpa-best-practice/src/main/java/vn/quynv/springframework/profiler/RepositoryProfiler.java
@@ -0,0 +1,33 @@
+package vn.quynv.springframework.profiler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class RepositoryProfiler {
+
+        @Pointcut("execution(public * org.springframework.data.repository.Repository+.*(..))")
+        public void intercept(){}
+
+        @Around("intercept()")
+        public Object profile(ProceedingJoinPoint joinPoint) {
+            long startMs = System.currentTimeMillis();
+            Object result = null;
+            try {
+                result = joinPoint.proceed();
+            }catch (Throwable e)
+            {
+                log.error(e.getMessage(), e);
+            }
+            long elapsedMs = System.currentTimeMillis() - startMs;
+            log.info(joinPoint.getTarget() + "." + joinPoint.getSignature()+ ": Execute time: " + elapsedMs+" (ms) ");
+            return result;
+
+        }
+}
