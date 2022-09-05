50774f8628c1ca5d9fa4b4491a2a88e41f214f9e
quynv86 <quynv86@gmail.com>
2022-09-05 11:48:44 +0700

    Optimize JPA by using Criteria

--- spring-core/src/test/java/vn/quynv/springframework/Utils.java
+++
@@ -1,9 +0,0 @@
-package vn.quynv.springframework;
-
-import java.util.List;
-
-public class Utils {
-    public static Integer max(List<Integer> values) {
-        return max;
-    }
-}
---
+++ spring-data-jpa-best-practice/src/test/java/vn/quynv/springframework/service/JPATest.java
@@ -0,0 +1,143 @@
+package vn.quynv.springframework.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import vn.quynv.springframework.entity.Author;
+import vn.quynv.springframework.entity.Bestseller;
+import vn.quynv.springframework.entity.Book;
+import vn.quynv.springframework.entity.Publisher;
+import vn.quynv.springframework.repository.AuthorRepository;
+import vn.quynv.springframework.repository.BestsellerRepository;
+import vn.quynv.springframework.repository.BookRepository;
+import vn.quynv.springframework.repository.PublisherRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+@SpringBootTest
+@Slf4j
+public class JPATest {
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private BookStoreService bookStoreService;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private BestsellerRepository bestsellerRepository;
+
+    @BeforeEach
+    void setup(){
+        Publisher publisher = publisherRepository.saveAndFlush(Publisher.builder().company("NXB - Kim Dong").authors(Collections.emptyList()).build());
+        Author author = authorRepository.saveAndFlush(Author.builder()
+                .books(Collections.emptyList())
+                .publisher(publisher).age(30).genre("Information Technology").name("Linux Toval").build());
+
+        Book book = Book.builder().isbn("ISBN-1").title("Spring Best Practice").author(author).build();
+        Book book_2 = Book.builder().isbn("ISBN-2").title("Hello Spring Boot").author(author).build();
+        bookRepository.saveAll(Arrays.asList(book, book_2));
+    }
+
+    @Test
+    void simpleQuery_Using_EntityManager(){
+        log.info("Start query all book using JPA API - Criteria - Query");
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Book> query = cb.createQuery(Book.class);
+        Root<Book> book = query.from(Book.class);
+        cb.selectCase(book);
+        query.where(
+                cb.like(book.get("title"),"Hello"),
+                cb.like(book.get("isbn"),"ISBN")
+                ,cb.or(
+                        cb.like(book.get("title"),"Hello"),
+                        cb.like(book.get("isbn"),"ISBN")
+                )
+        );
+        TypedQuery<Book> typedQuery = entityManager.createQuery(query);
+        typedQuery.getResultList().stream()
+                .forEach(bookEntity -> {
+                    log.info("Book: {}", bookEntity);
+
+                });
+    }
+
+    @Test
+    void query_With_InClause_Using_SubQuery() {
+        log.info("Query With In Clause Using Sub Query");
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Book> query = cb.createQuery(Book.class);
+        Root<Book> rBook = query.from(Book.class);
+        Subquery<Long> subQueryAuthorIds =query.subquery(Long.class);
+        Root<Bestseller> rBestseller = subQueryAuthorIds.from(Bestseller.class);
+        subQueryAuthorIds.select(rBestseller.get("authorId"));
+        Predicate withBestSellAuthor = rBook.get("author").get("id").in(subQueryAuthorIds);
+        query.select(rBook).where(cb.not(withBestSellAuthor));
+
+        TypedQuery<Book> typedQuery = entityManager.createQuery(query);
+        typedQuery.getResultList().stream().forEach((b) -> {
+            log.info("Got : {}", b);
+        });
+        // SQL Will be like :
+        /**
+         *  select
+         *         book0_.id as id1_2_,
+         *         book0_.deleted as deleted2_2_,
+         *         book0_.author_id as author_i5_2_,
+         *         book0_.isbn as isbn3_2_,
+         *         book0_.title as title4_2_
+         *     from
+         *         Book book0_
+         *     where
+         *         (
+         *             book0_.deleted = 0
+         *         )
+         *         and (
+         *             book0_.author_id in (
+         *                 select
+         *                     bestseller1_.authorId
+         *                 from
+         *                     Bestseller bestseller1_
+         *             )
+         *         )
+         */
+    }
+
+    @Test
+    void query_distinct_Title_Of_Book() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<String> query = cb.createQuery(String.class);
+        Root<Book> book = query.from(Book.class);
+        query.select(book.get("title")).distinct(true);
+        TypedQuery<String> typedQuery = entityManager.createQuery(query);
+        typedQuery.getResultList().stream().forEach((title) -> {
+            log.info("Got title: {}", title);
+        });
+    }
+    @Test
+    void query_Using_Cross_Join() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Book> criteriaQuery = cb.createQuery(Book.class);
+        Root<Book> rBook = criteriaQuery.from(Book.class);
+        Root<Bestseller> rBestSeller = criteriaQuery.from(Bestseller.class);
+        criteriaQuery.select(rBook)
+                .where(cb.equal(rBook.get("author").get("id"),rBestSeller.get("id"))
+                , cb.in(rBestSeller.get("id")).value(1));
+        entityManager.createQuery(criteriaQuery).getResultList()
+                .stream().forEach((b)-> {
+                    log.info("Got Book: {}", b);
+        });
+    }
+
+}
